e444c82cfe82ac496226734167c51b447d73a31a
Jessy Lespoir <lespoir.jessy@gmail.com>
2023-07-08 22:43:21 +0200

    Refactor country display and add filtering function

--- script.js
+++ script.js
@@ -1,77 +1,143 @@
 // Récupérer l'élément "donneesAPI" pour afficher les données
 const donneesAPI = document.getElementById('donneesAPI');
 
-// Créer une div pour contenir les éléments des pays
-let countries = document.createElement("div");
-countries.classList.add("pays");
+let data; // Déclarer la variable data
+
+// Ecouteur d'evenement liste déroulante
+let regionAfrica = document.getElementById('region-africa');
+let regionAmerica = document.getElementById('region-america');
+let regionAsia = document.getElementById('region-asia');
+let regionEurope = document.getElementById('region-europe');
+let regionOceania = document.getElementById('region-oceania');
 
 // Envoyer une requête GET à l'URL spécifiée
 fetch('https://restcountries.com/v3.1/all')
     // Convertir la réponse en format JSON
     .then(response => response.json())
-    .then(data => {
-        // Sélectionner 8 pays au hasard
-        let randomCountries = getRandomCountries(data, 8);
+    .then(responseData => {
+        data = responseData; // Assigner les données à la variable data
 
-        // Parcourir les pays sélectionnés et afficher leurs informations
-        randomCountries.forEach(country => {
-            // Créer une div pour chaque pays
-            let card = document.createElement('div');
-            card.classList.add('element-pays');
-
-            // Créer les éléments HTML pour afficher les informations du pays
-            let flag = document.createElement('img');
-            flag.src = country.flags.png;
-            flag.alt = 'Drapeau du pays';
-
-            let countryName = document.createElement('p');
-            countryName.textContent = country.name.common;
-
-            let population = document.createElement('p');
-            population.textContent = 'Population: ' + country.population;
-
-            let region = document.createElement('p');
-            region.textContent = 'Région: ' + country.region;
-
-            let capital = document.createElement('p');
-            capital.textContent = 'Capitale: ' + country.capital[0];
-
-            // Ajouter les éléments à la div du pays
-            card.appendChild(flag);
-            card.appendChild(countryName);
-            card.appendChild(population);
-            card.appendChild(region);
-            card.appendChild(capital);
-
-            // Ajouter la div du pays à la div des pays
-            countries.appendChild(card);
+        // Ajouter les écouteurs d'événements pour les filtres par continent
+        regionAfrica.addEventListener('click', () => {
+            displayCountriesByContinent('Africa');
         });
 
-        // Ajouter la div des pays à l'élément donneesAPI
-        donneesAPI.appendChild(countries);
+        regionAmerica.addEventListener('click', () => {
+            displayCountriesByContinent('Americas');
+        });
+
+        regionAsia.addEventListener('click', () => {
+            displayCountriesByContinent('Asia');
+        });
+
+        regionEurope.addEventListener('click', () => {
+            displayCountriesByContinent('Europe');
+        });
+
+        regionOceania.addEventListener('click', () => {
+            displayCountriesByContinent('Oceania');
+        });
+
+        // Afficher tous les pays au chargement de la page
+        displayAllCountries();
+
     })
     .catch(error => {
         // Gérer les erreurs
         console.log('Une erreur s\'est produite :', error);
     });
 
-// Fonction pour sélectionner des pays au hasard
-function getRandomCountries(countries, number) {
-    let randomCountries = [];
-    let indexes = [];
+// Fonction pour afficher tous les pays
+function displayAllCountries() {
+    // Supprime les pays affichés précédemment
+    donneesAPI.innerHTML = '';
 
-    // Générer des indexes uniques au hasard
-    while (indexes.length < number) {
-        let index = Math.floor(Math.random() * countries.length);
-        if (!indexes.includes(index)) {
-            indexes.push(index);
-        }
-    }
+    // Créer une div pour contenir les éléments des pays
+    let countries = document.createElement("div");
+    countries.classList.add("pays");
 
-    // Sélectionner les pays correspondant aux indexes
-    indexes.forEach(index => {
-        randomCountries.push(countries[index]);
+    // Parcourir les pays et afficher leurs informations
+    data.forEach(country => {
+        // Créer une div pour chaque pays
+        let card = document.createElement('div');
+        card.classList.add('element-pays');
+
+        let flag = document.createElement('img');
+        flag.src = country.flags.png;
+        flag.alt = 'Drapeau du pays';
+
+        let countryName = document.createElement('p');
+        countryName.textContent = country.name.common;
+
+        let population = document.createElement('p');
+        population.textContent = 'Population: ' + country.population;
+
+        let region = document.createElement('p');
+        region.textContent = 'Région: ' + country.region;
+
+        let capital = document.createElement('p');
+        capital.textContent = 'Capitale: ' + country.capital;
+
+        // Ajouter les éléments à la div du pays
+        card.appendChild(flag);
+        card.appendChild(countryName);
+        card.appendChild(population);
+        card.appendChild(region);
+        card.appendChild(capital);
+
+        // Ajouter la div du pays à la div des pays
+        countries.appendChild(card);
     });
 
-    return randomCountries;
+    // Ajouter la div des pays à l'élément donneesAPI
+    donneesAPI.appendChild(countries);
+}
+
+// Fonction pour afficher les pays par continent
+function displayCountriesByContinent(continent) {
+    // Supprime les pays affichés précédemment
+    donneesAPI.innerHTML = '';
+
+    // Créer une div pour contenir les éléments des pays
+    let countries = document.createElement("div");
+    countries.classList.add("pays");
+
+    // Filtrer les pays correspondant au continent sélectionné
+    let countriesByContinent = data.filter(country => country.region === continent);
+
+    // Parcourir les pays filtrés et afficher leurs informations
+    countriesByContinent.forEach(country => {
+        // Créer les éléments HTML pour afficher les informations du pays
+        let card = document.createElement('div');
+        card.classList.add('element-pays');
+
+        let flag = document.createElement('img');
+        flag.src = country.flags.png;
+        flag.alt = 'Drapeau du pays';
+
+        let countryName = document.createElement('p');
+        countryName.textContent = country.name.common;
+
+        let population = document.createElement('p');
+        population.textContent = 'Population: ' + country.population;
+
+        let region = document.createElement('p');
+        region.textContent = 'Région: ' + country.region;
+
+        let capital = document.createElement('p');
+        capital.textContent = 'Capitale: ' + country.capital;
+
+        // Ajouter les éléments à la div du pays
+        card.appendChild(flag);
+        card.appendChild(countryName);
+        card.appendChild(population);
+        card.appendChild(region);
+        card.appendChild(capital);
+
+        // Ajouter la div du pays à la div des pays
+        countries.appendChild(card);
+    });
+
+    // Ajouter la div des pays à l'élément donneesAPI
+    donneesAPI.appendChild(countries);
 }
